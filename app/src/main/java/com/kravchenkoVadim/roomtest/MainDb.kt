7b784c73a44b4cfc220eae2e6a4e495fa9628c27
Arhizmei <arhizmei@gmail.com>
2023-12-22 18:54:49 +0200

    add MainDb and table

---
+++ app/src/main/java/com/kravchenkoVadim/roomtest/MainDb.kt
@@ -0,0 +1,19 @@
+package com.kravchenkoVadim.roomtest
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database ()
+abstract class MainDb:RoomDatabase() {
+    companion object{
+        fun getDb(context: Context): MainDb{
+            return Room.databaseBuilder(
+                context.applicationContext,
+                MainDb::class.java,
+                "test.db"
+            ).build()
+        }
+    }
+}
