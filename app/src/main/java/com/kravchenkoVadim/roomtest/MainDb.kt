2a8e5f9d37759e8e11471f10899d1b616af3d973
Arhizmei <arhizmei@gmail.com>
2023-12-23 15:33:47 +0200

    try found error

--- app/src/main/java/com/kravchenkoVadim/roomtest/MainDb.kt
+++ app/src/main/java/com/kravchenkoVadim/roomtest/MainDb.kt
@@ -4,17 +4,25 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database (entities = [Item::class], version = 1)
-abstract class MainDb:RoomDatabase() {
-    abstract fun getDao():Dao
-    companion object{
-        fun getDb(context: Context): MainDb{
+@Database(entities = [Item::class], version = 2)
+abstract class MainDb : RoomDatabase() {
+    abstract fun getDao(): Dao
+
+    companion object {
+        fun getDb(context: Context): MainDb {
             return Room.databaseBuilder(
                 context.applicationContext,
                 MainDb::class.java,
                 "test.db"
-            ).build()
+            ).addMigrations(MIGRATION_1_2).build()
+        }
+        private val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                // Твои изменения схемы базы данных
+            }
         }
     }
 }
