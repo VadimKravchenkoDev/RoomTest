df709d9d336dfab38fa446681bf04f0785f6d1c1
Arhizmei <arhizmei@gmail.com>
2023-12-23 16:06:28 +0200

    1.2

--- app/src/main/java/com/kravchenkoVadim/roomtest/MainActivity.kt
+++ app/src/main/java/com/kravchenkoVadim/roomtest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kravchenkoVadim.roomtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.asLiveData
 import com.kravchenkoVadim.roomtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val db = MainDb.getDb(this)
+        db.getDao().getAllItem().asLiveData().observe(this) {list->
+            //отсюда загружать в RecycleView
+            binding.tvText.text = ""
+            list.forEach {
+                val text = "Id : ${it.id} Name : ${it.name} Price : ${it.price}\n"
+                binding.tvText.append(text)
+            }
+        }
         binding.button.setOnClickListener {
             val item = Item(
                 null,
